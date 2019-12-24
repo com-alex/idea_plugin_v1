2ea0ffed791cf46500c3ba224cf2d86d6c206364
alex <5791159@qq.com>
2019-12-24 18:42:04 +0800

    init project

---
+++ src/test/java/com/fromLab/TestProject.java
@@ -0,0 +1,15 @@
+package com.fromLab;
+
+import org.junit.Test;
+
+/**
+ * @author wsh
+ * @date 2019-12-24
+ */
+public class TestProject {
+
+    @Test
+    public void test(){
+        System.out.println(1);
+    }
+}
