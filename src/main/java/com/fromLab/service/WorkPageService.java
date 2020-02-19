3f8bfe1bea1869099ce46614e2d15e2e0baa56a7
baojiaang <574906782@qq.com>
2020-02-19 10:52:19 +0800

    Default Changelist

--- src/main/java/com/fromLab/service/WorkPageService.java
+++ src/main/java/com/fromLab/service/WorkPageService.java
@@ -6,6 +6,7 @@ import com.fromLab.entity.Status;
 import com.fromLab.entity.WorkPage;
 
 
+import java.util.Date;
 import java.util.List;
 
 public interface WorkPageService {
@@ -13,5 +14,8 @@ public interface WorkPageService {
     WorkPage updateStatus(String openprojectURL, String apikey, int id, Status status, int lock_version);
     WorkPage getTaskById(String openprojectURL, String apiKey, int id);
     WorkPage UpdatePercentageDone(String openprojectURL, String apiKey, int id, int lock_version, int percentage);
+    WorkPage updateStartDate(String openprojectURL, String apiKey, int id, int lock_version, String start_date);
+    WorkPage updateEndDate(String openprojectURL, String apiKey, int id, int lock_version, String end_date,String customField );
+    WorkPage updateSpentTime(String openprojectURL, String apiKey, int id, int lock_version, int time,String customField );
    // Task updateLocalProperties(String openprojectURL, String apiKey, HashMap<Object,Object> properties,int lock_version);
 }
